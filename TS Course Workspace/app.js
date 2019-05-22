4e4a7a52c5627b4fcd3c11c5c7101efef6e0b259
DESKTOP-ULQ3D3M\Andre <andriisymonenko@gmail.com>
2019-05-22 22:11:53 +0300

    Section 2: Types

--- TS Course Workspace/app.js
+++ TS Course Workspace/app.js
@@ -1,1 +1,33 @@
-console.log("It works!");
+var _this = this;
+// tuples
+var address = ['Superstreet', 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["White"] = 2] = "White";
+})(Color || (Color = {}));
+function sayHello() {
+    console.log('Hello');
+}
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// function types
+var myMultiply;
+myMultiply = multiply;
+console.log(myMultiply);
+var userData = {
+    name: 'Andrey',
+    age: 27
+};
+var complex = {
+    data: [1, 2, 3],
+    output: function (all) {
+        return _this.data;
+    }
+};
+// Union Types
+var myRealRealAge = 27;
+console.log(typeof myRealRealAge);
