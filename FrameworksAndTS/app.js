97bc1318fc6912c2472dcd7f903e8d7c3cbaf018
DESKTOP-ULQ3D3M\Andre <andriisymonenko@gmail.com>
2019-05-25 17:48:51 +0300

    Section10: Using JavaScript Libraries (like jQuery) with TypeScript

---
+++ FrameworksAndTS/app.js
@@ -0,0 +1,4 @@
+"use strict";
+$('button').on('click', function () {
+    console.log('Btn was clicked!');
+});
---
+++ FrameworksAndTS/app.ts
@@ -0,0 +1,4 @@
+//declare var $: any;
+$('button').on('click', function() {
+  alert('Btn was clicked!');
+});
